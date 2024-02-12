023118c9fe34644b8cc29554c8568e5182f10509
Alex Bilson <acbilson@gmail.com>
2024-02-12 09:47:04 -0700

    adds first db models

--- savoy/menu/models.py
+++ savoy/menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MenuSection(models.Model):
+    title_txt = models.CharField(max_length=100)
+
+class MenuItem(models.Model):
+    title_txt = models.CharField(max_length=100)
+    desc_txt = models.CharField(max_length=200)
+    heat_rating = models.IntegerField(default=0)
+    section = models.ForeignKey(MenuSection, on_delete=models.CASCADE)
