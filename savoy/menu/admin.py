50d8a0640653fe6c44c6c667bc1eb1fe06a342c7
Alex Bilson <acbilson@gmail.com>
2024-02-12 09:50:45 -0700

    adds models to admin

--- savoy/menu/admin.py
+++ savoy/menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MenuSection, MenuItem
+
+admin.site.register(MenuSection)
+admin.site.register(MenuItem)
+
--- savoy/menu/models.py
+++ savoy/menu/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class MenuSection(models.Model):
     title_txt = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title_txt
+
 class MenuItem(models.Model):
     title_txt = models.CharField(max_length=100)
     desc_txt = models.CharField(max_length=200)
     heat_rating = models.IntegerField(default=0)
     section = models.ForeignKey(MenuSection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title_txt
